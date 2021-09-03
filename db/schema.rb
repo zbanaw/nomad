e7d2058bd5a724c857ab6e4ec4603929a110ed63
Anthony Zban <anthony.zban+github@gmail.com>
2021-09-02 22:21:07 -0400

    Add seed dataset

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_02_191632) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "available_times", force: :cascade do |t|
+    t.bigint "coach_id", null: false
+    t.integer "day_of_week"
+    t.time "start"
+    t.time "end"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coach_id"], name: "index_available_times_on_coach_id"
+  end
+
+  create_table "coaches", force: :cascade do |t|
+    t.string "name"
+    t.string "timezone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "available_times", "coaches"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'coaches.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  # To normalize data
+  c = Coach.find_or_create_by!(
+    name: row['Name'],
+    timezone: row['Timezone'][12..-1] # Remove formatted_offset
+  )
+  tz = ActiveSupport::TimeZone[c.timezone]
+
+  a = AvailableTime.new
+  a.coach_id = c.id
+  a.day_of_week = row["Day of Week"].to_date.wday # Day as integers
+  a.start = tz.local_to_utc(row["Available at"].to_time) # Date to UTC
+  a.end = tz.local_to_utc(row["Available until"].to_time) # Date to UTC
+  a.save!
+end
+
+puts "#{Coach.count} Coaches added"
+puts "#{AvailableTime.count} Available Times added"
