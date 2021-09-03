be10c05585f792586981bbcca289d45c28a28a7e
Anthony Zban <anthony.zban+github@gmail.com>
2021-09-03 08:00:58 -0400

    Add method to create timeslots

--- app/models/available_time.rb
+++ app/models/available_time.rb
@@ -4,4 +4,12 @@ class AvailableTime < ApplicationRecord
   validates :day_to_week, presence: true
   validates :start, presence: true
   validates :end, presence: true
+
+  def time_slot_array
+    options = []
+    (self.start.to_i...self.end.to_i).step(30.minutes).each do |time|
+      options.push Time.at(time).in_time_zone(ActiveSupport::TimeZone[coach.timezone]).strftime("%l:%M %p")
+    end
+    options
+  end
 end
