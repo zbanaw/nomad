f44b36594c9f2395561bb2c632e80e59f8beb125
Anthony Zban <anthony.zban+github@gmail.com>
2021-09-02 22:25:17 -0400

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_191632) do
+ActiveRecord::Schema.define(version: 2021_09_02_203405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2021_09_02_191632) do
   create_table "coaches", force: :cascade do |t|
     t.string "name"
     t.string "timezone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
